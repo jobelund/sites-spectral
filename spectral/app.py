285fac96f72cc33d3e9248dcc4b174d0cd11f7b6
José Beltrán <102664984+jose-begeospatial@users.noreply.github.com>
2023-06-02 13:43:11 +0200

    WIP

--- spectral/app.py
+++ spectral/app.py
@@ -1,7 +1,13 @@
+import os
 from pathlib import Path
 import streamlit as st
 
+
 st.set_page_config(layout='wide')
 
+# The following must retrieve path: `/home/sites/spectral/spectral`
 APP_DIRPATH = Path(__file__).resolve().parents[0]
+
+os.path.join(ASSETS_DIRPATH, 'SITESspectral_LOGO_254x251.png')
+
 st.write(APP_DIRPATH)
